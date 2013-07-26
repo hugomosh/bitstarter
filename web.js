9df790202d8fe29be12475bdfbcb468c0fc98ed5
hugomosh <hugomosh@gmail.com>
2013-07-26 16:16:31 +0000

    Change web.js to fs 3

--- web.js
+++ web.js
@@ -2,11 +2,11 @@ var express = require('express');
 var fs = requiere ('fs');
 var app = express();
 var buffer = fs.readFileSync('/index.html');
-console.debug(buffer.toString('utf8'));
+console.debug(buffer.toString('utf-8'));
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf8'));
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
